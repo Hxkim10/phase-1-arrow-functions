42dd44b2cda7d25325d6d85bf94e4909e3d98d5d
hxkim10 <abdelhakimtiabi@gmail.com>
2023-08-13 15:43:25 +0100

    Completed Assignement

---
+++ index.js
@@ -0,0 +1,10 @@
+///1 ///
+const divide = function () {
+  return 2000 / 100;
+};
+
+/// 2 ///
+const square = (number) => number * number;
+
+/// 3 ///
+const add = (number1, number2) => number1 + number2;
